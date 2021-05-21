6f6fb2c35d82ba1be0a8b2ff9b441c846f4e0f45
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-21 23:44:00 +0300

    Add details to show in the repos

--- src/app/repo-class.ts
+++ src/app/repo-class.ts
@@ -1,2 +1,12 @@
 export class RepoClass {
+    constructor (
+        public name:string,
+        public updated_at:Date,
+        // public private:boolean,
+        public html_url:string,
+        public clone_url:string,
+        public git_url:string,
+        public pushed_at:string,
+        public collaborators_url:string
+    ){}
 }
