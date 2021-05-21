794cd42695ed9c0b71b3c6d644ccdb3fb397e2c5
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-21 23:58:52 +0300

    Import enviroments to service

--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@angular/core';
 //import httpClient --->>http
 import {HttpClient} from '@angular/common/http';
 //import enviroment
+import {environment} from '../environments/environment';
 //import user details
+import {UserDetails} from './user-details'
 //import repos
+import {RepoClass} from './repo-class';
 @Injectable({
   providedIn: 'root'
 })
