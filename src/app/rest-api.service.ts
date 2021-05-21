184e96fa13cd4d44b348b97236e31168e5ca0819
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-21 23:53:19 +0300

    Import httpClient in services

--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-
+//import httpClient --->>http
+import {HttpClient} from '@angular/common/http';
+//import enviroment
+//import user details
+//import repos
 @Injectable({
   providedIn: 'root'
 })
